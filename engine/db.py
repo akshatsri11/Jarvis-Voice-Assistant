df07d9b0d34ff01a56a0213882b83a1747913c63
akshatsri11 <akshat13579@gmail.com>
2024-10-14 03:16:19 +0530

    creating database

--- engine/db.py
+++ engine/db.py
@@ -5,3 +5,17 @@ cursor = conn.cursor()
 
 query = "CREATE TABLE IF NOT EXISTS sys_command(id INTEGER PRIMARY KEY, name VARCHAR(255), path VARCHAR(1000))"
 cursor.execute(query)
+
+# query = "INSERT INTO sys_command VALUES(null, 'one note', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.exe')"
+# cursor.execute(query)
+
+# conn.commit()
+
+
+query = "CREATE TABLE IF NOT EXISTS web_command(id INTEGER PRIMARY KEY, name VARCHAR(255), url VARCHAR(1000))"
+cursor.execute(query)
+
+query = "INSERT INTO web_command VALUES(null, 'youtube', 'https://www.youtube.com/')"
+cursor.execute(query)
+
+conn.commit()
